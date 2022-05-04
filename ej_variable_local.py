5fcf69efc0a1e6b0564299f31c9f247c8d2cfc08
Jordi Calaf conK <>
2022-05-04 07:53:15 +0000

    Initial commit

---
+++ ej_variable_local.py
@@ -0,0 +1,9 @@
+def subrutina():
+    variable = 10
+    print(variable)
+    return
+  
+variable = 20
+subrutina()
+print(variable)
+subrutina()
---
+++ main.py
@@ -0,0 +1,31 @@
+
+import keyword
+
+print(keyword)
+print(keyword.kwlist)
+
+variable = 10
+print(variable)
+
+def funcion():
+    x=5
+    return x
+print(funcion())
+
+nombre = "Jordi"
+print("Hola",nombre)
+
+print("Esto es todo", end=" ")
+print("Amigos!!!")
+
+print("Este","lenguaje","se",sep="_",end="*")
+print("denomina","Python.",sep="*",end="*\n")
+
+tipos = "Hola mundo!"
+print("Identidad:",id(tipos))
+print("Tipo:", type(tipos))
+print("Valor:", tipos)
+
+
+
+
